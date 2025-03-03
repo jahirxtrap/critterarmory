d71c257487084b64ad54c6edb0eb7a7a9f1da4a6
jahirxtrap <jahirroman2002@gmail.com>
2025-03-03 14:28:21 -0500

    v0.4.3

--- fabric/src/main/java/com/jahirtrap/critterarmory/init/ModConfig.java
+++ fabric/src/main/java/com/jahirtrap/critterarmory/init/ModConfig.java
@@ -10,7 +10,7 @@ public class ModConfig extends TXFConfig {
     @Entry(name = "Health Increase Amount", min = 0, max = Integer.MAX_VALUE, itemDisplay = "critterarmory:vitality_feed")
     public static int healthIncreaseAmount = 6;
     @Comment(centered = true)
-    public static Comment armors;
+    public static Comment rendering;
     @Entry(name = "Render Armors", itemDisplay = "critterarmory:netherite_pig_armor")
     public static boolean renderArmors = true;
     @Entry(name = "Render Horse Armors", itemDisplay = "critterarmory:netherite_horse_armor")
@@ -25,4 +25,6 @@ public class ModConfig extends TXFConfig {
     public static boolean renderPigArmors = true;
     //@Entry(name = "Render Sheep Armors", itemDisplay = "critterarmory:netherite_sheep_armor")
     public static boolean renderSheepArmors = false;
+    @Entry(name = "Modern Wolf Collar", itemDisplay = "minecraft:bone")
+    public static boolean modernWolfCollar = true;
 }
