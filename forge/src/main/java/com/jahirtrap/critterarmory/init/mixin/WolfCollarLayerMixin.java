3af39af74f7566051f14b7142a5f06e209779515
jahirxtrap <jahirroman2002@gmail.com>
2025-03-03 14:30:12 -0500

    v0.4.3

---
+++ forge/src/main/java/com/jahirtrap/critterarmory/init/mixin/WolfCollarLayerMixin.java
@@ -0,0 +1,26 @@
+package com.jahirtrap.critterarmory.init.mixin;
+
+import com.jahirtrap.critterarmory.init.ModConfig;
+import net.minecraft.client.renderer.entity.layers.WolfCollarLayer;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static com.jahirtrap.critterarmory.CritterArmoryMod.MODID;
+
+@Mixin(WolfCollarLayer.class)
+public abstract class WolfCollarLayerMixin {
+
+    @Final
+    @Shadow
+    private static ResourceLocation WOLF_COLLAR_LOCATION;
+
+    @Redirect(method = "render*", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/entity/layers/WolfCollarLayer;WOLF_COLLAR_LOCATION:Lnet/minecraft/resources/ResourceLocation;"))
+    private ResourceLocation redirectWolfCollarTexture() {
+        if (!ModConfig.modernWolfCollar) return new ResourceLocation(MODID, "textures/entity/wolf/wolf_collar.png");
+        else return WOLF_COLLAR_LOCATION;
+    }
+}
