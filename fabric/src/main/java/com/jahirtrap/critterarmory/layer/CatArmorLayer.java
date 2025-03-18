5d6e03ce557a5aeb24a8a6c98455beb4601c022e
jahirxtrap <jahirroman2002@gmail.com>
2025-03-18 14:29:07 -0500

    Cat armor

---
+++ fabric/src/main/java/com/jahirtrap/critterarmory/layer/CatArmorLayer.java
@@ -0,0 +1,38 @@
+package com.jahirtrap.critterarmory.layer;
+
+import com.jahirtrap.critterarmory.init.ModModelLayers;
+import com.jahirtrap.critterarmory.item.BaseAnimalArmorItem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.model.CatModel;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.item.ItemStack;
+
+import static com.jahirtrap.critterarmory.util.CommonUtils.renderArmor;
+
+@Environment(EnvType.CLIENT)
+public class CatArmorLayer extends RenderLayer<Cat, CatModel<Cat>> {
+    private final CatModel<Cat> model;
+
+    public CatArmorLayer(RenderLayerParent<Cat, CatModel<Cat>> layerParent, EntityModelSet modelSet) {
+        super(layerParent);
+        this.model = new CatModel<>(modelSet.bakeLayer(ModModelLayers.CAT_ARMOR));
+    }
+
+    @Override
+    public void render(PoseStack poseStack, MultiBufferSource bufferSource, int i, Cat entity, float f, float g, float h, float j, float k, float l) {
+        ItemStack stack = entity.getItemBySlot(EquipmentSlot.CHEST);
+        if (stack.getItem() instanceof BaseAnimalArmorItem.Modded animalArmorItem) {
+            this.getParentModel().copyPropertiesTo(this.model);
+            this.model.prepareMobModel(entity, f, g, h);
+            this.model.setupAnim(entity, f, g, j, k, l);
+            renderArmor(animalArmorItem.getMaterial().getLocation(), model, stack, poseStack, bufferSource, i);
+        }
+    }
+}
